a5b35c9c1a26977a7295e5753db118ed3f7fafc0
Didac-piferrer <didacpiferrer@hotmail.com>
2022-12-04 08:41:13 +0100

    CH4-p1

---
+++ CH4/CH-4.js
@@ -0,0 +1,16 @@
+const { count } = require('console');
+var fs = require('fs')
+var archivo = fs.readFileSync( "input.txt", "utf-8")
+var nuevoArchivo = archivo.split("\n")
+var contador = 0
+nuevoArchivo.forEach(element => {
+    var elf1 = element.split(",")[0]
+    var elf2 = element.split(",")[1]
+    if (typeof elf1[0] != "undefined"){ // por si hay lineas vacías
+        var min1 = Number(elf1.split("-")[0]), min2 = Number(elf2.split("-")[0]), max1 = Number(elf1.split("-")[1]), max2 = Number(elf2.split("-")[1])
+        if ((min1 >= min2 && max1 <= max2) || (min2 >= min1 && max2 <= max1)){
+            contador++
+        }
+    }
+});
+console.log(contador)
