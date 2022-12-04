c3865e0c11a40f6d7ded10f7efee0f80b507b69c
Didac-piferrer <didacpiferrer@hotmail.com>
2022-12-04 08:53:44 +0100

    ch4 parte 2

--- CH4/CH-4-2.js
+++ CH4/CH-4-2.js
@@ -7,7 +7,7 @@ nuevoArchivo.forEach(element => {
     var elf2 = element.split(",")[1]
     if (typeof elf1[0] != "undefined"){ // por si hay lineas vacías
         var min1 = Number(elf1.split("-")[0]), min2 = Number(elf2.split("-")[0]), max1 = Number(elf1.split("-")[1]), max2 = Number(elf2.split("-")[1])
-        if ((min1 >= min2 && max1 <= max2) || (min2 >= min1 && max2 <= max1)){
+        if (!(max1 < min2 || max2 < min1)) {
             contador++
         }
     }
